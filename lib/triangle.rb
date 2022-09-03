33fa63098e59d2d0c3c2d522b78f437be8db8ce1
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-09-04 00:00:45 +0300

    commit tree c21c8fed7ea0a2e117e9328149c933dc0a1634e7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  def initialize (side_one, side_two, side_three)
+  @side_one = side_one
+  @side_two = side_two
+  @side_three = side_three
+  end
+
+  def kind
+    if is_valid?
+      if @side_one == @side_two && @side_two == @side_three
+        return :equilateral
+      elsif @side_one == @side_two || @side_one == @side_three || @side_two == @side_three
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+  class TriangleError < StandardError
+
+  end
+
+  #private
+
+  def is_valid?
+    unless @side_one <= 0 || @side_two <=0 || @side_three<=0 || @side_one + @side_two <= @side_three || @side_two + @side_three <= @side_one || @side_one + @side_three <= @side_two
+      return true
+    end
+  end
+
 end
+
